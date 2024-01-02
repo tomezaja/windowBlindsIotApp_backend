4473da9442bf27203f68d82ecee90a4ead933f4d
Tome Žaja <tome.zaja@backend.hr>
2024-01-02 15:08:02 +0100

    init commit

---
+++ src/main/java/windowBlindsIoTApp/WindowBlindsApplication.java
@@ -0,0 +1,13 @@
+package windowBlindsIoTApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowBlindsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowBlindsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/windowBlindsIoTApp/WindowBlindsApplicationTests.java
@@ -0,0 +1,13 @@
+package windowBlindsIoTApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowBlindsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
